51930327a12bea5e562eb01443484622ec2bdf61
Manvendra Bhadauria <manvendra.bhadau@tigeranalytics.local>
2024-01-04 15:19:12 +0530

    color change of side bar when open

--- src/style.py
+++ src/style.py
@@ -61,6 +61,9 @@ padding-top:7px;
 [class="eyeqlp51 st-emotion-cache-fblp2m ex0cdmw0"]{
 color:white;
 }
+[d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"]{
+color:Black;
+}
 
 </style>
 """
