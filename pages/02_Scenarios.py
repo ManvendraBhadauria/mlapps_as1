645ae7a2900c77f9ced6bb153046ef9c25cb0304
Manvendra Bhadauria <manvendra.bhadau@tigeranalytics.local>
2024-01-18 13:00:36 +0530

    Making the charts size dynamic

--- pages/02_Scenarios.py
+++ pages/02_Scenarios.py
@@ -19,7 +19,15 @@ st.markdown(css, unsafe_allow_html=True)
 
 @st.cache_data
 def load_data():
-    selected_cols = ["Name", "Created Date", "Created by", "revenue", "cost", "profit", "prec_profit"]
+    selected_cols = [
+        "Name",
+        "Created Date",
+        "Created by",
+        "revenue",
+        "cost",
+        "profit",
+        "prec_profit",
+    ]
     data = pd.read_excel("data/scenarios.xlsx", sheet_name=0)
     return data[selected_cols]
 
@@ -32,10 +40,15 @@ builder.configure_selection(selection_mode="multiple", use_checkbox=True)
 builder.configure_default_column(min_column_width=50)
 go = builder.build()
 
-grid_table = AgGrid(data=df, gridOptions=go, update_mode=GridUpdateMode.SELECTION_CHANGED, allow_unsafe_jscode=True)
+grid_table = AgGrid(
+    data=df,
+    gridOptions=go,
+    update_mode=GridUpdateMode.SELECTION_CHANGED,
+    allow_unsafe_jscode=True,
+)
 selected_rows = grid_table["selected_rows"]
 
 st.markdown("---")
 if len(selected_rows) != 0:
     bar_Chart = scenario_bar_chart(selected_rows)
-    st.plotly_chart(bar_Chart)
+    st.plotly_chart(bar_Chart, use_container_width=True)
